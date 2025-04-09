9a4ba11074418c902f6e4347bdb1164a4c83205a
YassineBen <yassine8ben@gmail.com>
2025-04-09 11:39:55 +0100

    first commit

---
+++ src/components/HeaderLeft.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderLeft = ({ darkMode, toggleDarkMode }) => {
+  return <div className="min-h-screen flex-1">HeaderLeft</div>;
+};
+
+export default HeaderLeft;
